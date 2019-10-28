8e844f7427f8e893617585913681824bb42ae72b
HUANGZHE <huangzhe@kingsoft.com>
2019-10-28 16:11:51 +0800

    更改story-cli readme为默认右边

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,18 @@
-import {configure} from '@storybook/react';
+import {configure, addParameters} from '@storybook/react';
 import createStory from './stories-helper';
 
+addParameters({
+    options: {
+        panelPosition: 'right'
+    }
+});
 
 function loadStories() {
-  const req = require.context('@src', true, /\.story\.js$/);
-  return req.keys().map(filename => {
-    const params = req(filename).default;
-    createStory.apply(null, params);
-  });
+    const req = require.context('@src', true, /\.story\.js$/);
+    return req.keys().map(filename => {
+        const params = req(filename).default;
+        createStory.apply(null, params);
+    });
 }
 
 configure(loadStories, module);
